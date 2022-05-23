826ca0b8fe6da705366ce601e0bb62b58562d473
Doron Segal <doron2402@gmail.com>
2022-05-23 13:26:55 +0300

    remove ugly header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,22 +8,6 @@ type Props = {
 
 const Layout = ({ children }: Props) => (
   <div>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
     {children}
     <footer>
       <hr />
